92c439e9297137eaea3a9873681a23b41e215bc2
wault404 <57727805+wault404@users.noreply.github.com>
2023-12-20 06:26:19 +0100

    Add files via upload

--- Analyzer.py
+++ Analyzer.py
@@ -1,59 +1,99 @@
-from scapy.all import sniff
+from scapy.all import sniff, conf
 from scapy.layers.inet import IP
-from scapy.config import conf
-import socket
 import geoip2.database
+from ipwhois import IPWhois
+import tkinter as tk
+from tkinter import ttk
+from datetime import datetime, timedelta
 
 conf.use_pcap = True
 
 class PacketSniffer:
-    def __init__(self, target_ip):
+    def __init__(self, target_ip, capture_duration_minutes=1):
         self.target_ip = target_ip
+        self.capture_duration = timedelta(minutes=capture_duration_minutes)
+        self.start_time = datetime.now()
         self.reader = geoip2.database.Reader(r'C:\Users\Wault404\Desktop\python\SOCAnalyze\GeoLite2-City_20231110\GeoLite2-City.mmdb')
-    def resolve_ip_name(self, ip_address):
-        try:
-            host_name, _, _ = socket.gethostbyaddr(ip_address)
-            return host_name
-        except socket.herror:
-            return "N/A"
+        self.geoip_results = {}
 
     def packet_callback(self, packet):
-        packet_summary = f"Summary: {packet.summary()}"
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        packet_size = len(packet)
 
-        src_name = self.resolve_ip_name(packet[IP].src)
-        dst_name = self.resolve_ip_name(packet[IP].dst)
-        packet_summary += f"\nSource IP: {packet[IP].src} ({src_name}), Destination IP: {packet[IP].dst} ({dst_name})"
-
-
-        packet_summary += f"\nPacket Size: {len(packet)} bytes"
-
-        geoip_info = self.get_geoip_info(packet[IP].src)
-        packet_summary += f"\nGeoIP: {geoip_info}"
-
-        print(packet_summary)
-
-    def get_geoip_info(self, ip_address):
         try:
-            response = self.reader.city(ip_address)
+            response = self.reader.city(src_ip)
             country = response.country.name
             city = response.city.name
-            return f"Country: {country}, City: {city}"
+
+            ipwhois = IPWhois(src_ip)
+            result = ipwhois.lookup_rdap()
+            as_info = result.get('asn_description', 'N/A')
+
+            geoip_info = f"Country: {country}, City: {city}"
         except geoip2.errors.AddressNotFoundError:
-            return "GeoIP information not available"
+            geoip_info = "GeoIP information not available"
+            as_info = "N/A"
         except Exception as e:
-            return f"Error retrieving GeoIP information: {e}"
+            geoip_info = f"Error retrieving GeoIP information: {e}"
+            as_info = "N/A"
+
+        if as_info == "arin-pfs-sea":
+            as_info = "Custom AS Organization: arin-pfs-sea"
+
+        key = (src_ip, dst_ip)
+
+        if key in self.geoip_results:
+            self.geoip_results[key]['Packet Size'] += packet_size
+            self.geoip_results[key]['Number of Packets'] += 1
+        else:
+            self.geoip_results[key] = {
+                'Source IP': src_ip,
+                'Destination IP': dst_ip,
+                'Packet Size': packet_size,
+                'GeoIP Information': geoip_info,
+                'AS Organization': as_info,
+                'Number of Packets': 1
+            }
+
+        elapsed_time = datetime.now() - self.start_time
+        if elapsed_time >= self.capture_duration:
+            self.stop_capture()
 
     def start_capture(self):
         try:
             sniff(prn=self.packet_callback, filter=f"host {self.target_ip}", store=0)
         except KeyboardInterrupt:
-            print("\nExiting...")
+            self.stop_capture()
 
-    def on_stop(self):
+    def stop_capture(self):
+        print("\nStopping capture...")
         self.reader.close()
+        self.display_geoip_table()
 
+    def display_geoip_table(self):
+        root = tk.Tk()
+        root.title("GeoIP Information")
+
+        tree = ttk.Treeview(root)
+        tree["columns"] = ("Source IP", "Destination IP", "Packet Size", "Number of Packets", "GeoIP Information", "AS Organization")
+        for col in tree["columns"]:
+            tree.heading(col, text=col)
+        tree.pack(expand=True, fill=tk.BOTH)
+
+        for idx, (key, result) in enumerate(self.geoip_results.items()):
+            tree.insert("", idx, values=(
+                result['Source IP'],
+                result['Destination IP'],
+                result['Packet Size'],
+                result['Number of Packets'],
+                result['GeoIP Information'],
+                result['AS Organization']
+            ))
+
+        tree.mainloop()
 
 if __name__ == "__main__":
-    target_ip = "TvojIPsem"
+    target_ip = "192.168.0.103"
     packet_sniffer = PacketSniffer(target_ip)
     packet_sniffer.start_capture()
